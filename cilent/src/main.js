ef500751ad5f83534e17aafc23ef1bc8ef105b48
DanaiP62 <danaip62@nu.ac.th>
2021-09-04 04:44:28 +0700

    newblog

--- cilent/src/main.js
+++ cilent/src/main.js
@@ -3,10 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { sync } from 'vuex-router-sync'
-import store from  './store'
+import {sync} from 'vuex-router-sync'
+import store from './store'
 import VueResource from 'vue-resource'
+import BackHeader from '@/components/Header.vue'
 
+Vue.component('back-header', BackHeader)
 
 Vue.config.productionTip = false
 
